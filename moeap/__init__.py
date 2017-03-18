46ac192dadcbaadc7c80d7a7f5cbcf55353fb910
Mosen <mosen@users.noreply.github.com>
2017-03-18 23:36:08 +1100

    more commands

---
+++ moeap/__init__.py
@@ -0,0 +1,21 @@
+
+EAP_TYPE_DESCRIPTIONS = {
+    0: 'Invalid',
+    1: 'Identity',
+    2: 'Notification',
+    3: 'Nak',
+    4: 'MD5 Challenge',
+    5: 'One time password',
+    6: 'Generic token card',
+    13: 'TLS',
+    17: 'Cisco LEAP',
+    18: 'EAP-SIM',
+    19: 'SRP SHA-1',
+    21: 'TTLS',
+    23: 'EAP AKA',
+    25: 'PEAP',
+    26: 'MSCHAPv2',
+    33: 'Extensions',
+    43: 'EAP-FAST',
+    50: 'EAP AKA Prime'
+}
